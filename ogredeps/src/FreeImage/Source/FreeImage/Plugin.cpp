11e192f7c01d0f8fd290ce697a54189fc7b746c5
DylanCheetah <dylan_the_cheetah@outlook.com>
2026-05-24 23:40:42 -0400

    Disable TIFF and fix undefined references in FreeImage.

--- ogredeps/src/FreeImage/Source/FreeImage/Plugin.cpp
+++ ogredeps/src/FreeImage/Source/FreeImage/Plugin.cpp
@@ -246,7 +246,7 @@ FreeImage_Initialise(BOOL load_local_plugins_only) {
 			s_plugins->AddNode(InitPNM, NULL, "PPMRAW", "Portable Pixelmap (RAW)", "ppm", "^P6");
 			s_plugins->AddNode(InitRAS);
 			s_plugins->AddNode(InitTARGA);
-			s_plugins->AddNode(InitTIFF);
+			//s_plugins->AddNode(InitTIFF);
 			s_plugins->AddNode(InitWBMP);
 			s_plugins->AddNode(InitPSD);
 			s_plugins->AddNode(InitCUT);
@@ -257,7 +257,7 @@ FreeImage_Initialise(BOOL load_local_plugins_only) {
 	        s_plugins->AddNode(InitHDR);
 			s_plugins->AddNode(InitG3);
 			s_plugins->AddNode(InitSGI);
-			s_plugins->AddNode(InitEXR);
+			//s_plugins->AddNode(InitEXR);
 			s_plugins->AddNode(InitJ2K);
 			s_plugins->AddNode(InitJP2);
 			s_plugins->AddNode(InitPFM);
